b9bec7204a61cbc3bd8eb9a51e3c480f82289c7e
zanemmiller2 <millzane@oregonstate.edu>
2022-08-01 15:53:30 -0700

    camera screen

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -8,8 +10,45 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+
+  File? image;
+  final picker = ImagePicker();
+
+  void getImage() async {
+    // TODO Add logic to handle user not allowing permission to access photos
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    image = File(pickedFile!.path);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: CircularProgressIndicator());
+    if (image == null) {
+      return Center(
+          child: ElevatedButton(
+            onPressed: () {
+              getImage();
+
+            },
+            child: Text('Select Photo'),
+          )
+      );
+    } else {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.file(image!),
+            SizedBox(height: 40,),
+            ElevatedButton(
+                onPressed: () {
+
+                },
+                child: Text('Post It!')
+            )
+          ],
+        ),
+      );
+    }
   }
 }
