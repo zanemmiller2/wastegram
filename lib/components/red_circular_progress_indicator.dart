5b708b39f1287b5f3e8eab2315a421d518858b7f
zanemmiller2 <millzane@oregonstate.edu>
2022-08-02 11:01:20 -0700

    initialized firebase project

---
+++ lib/components/red_circular_progress_indicator.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class RedCircularProgressIndicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => const CircularProgressIndicator(color: Colors.red);
+}
